8946262c9b203585d377aee860689262d2a752d3
nisarkhokhar07 <nisar.khokar@focusteck.com>
2023-05-26 00:02:07 +0500

    added link to homepage to the title of Site

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <Link to='/home'>
+      <Link to='/'>
       <h1>Nisar's Blogs</h1>
       </Link>
 
